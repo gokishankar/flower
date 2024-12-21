d15fdadcf85bd0d61c93c74af2b2a57b1a8a1551
gokishankar <gokilathirumalaisamy@gmail.com>
2024-12-21 07:13:24 +0530

    messages

---
+++ src/org/java/java.java
@@ -0,0 +1,13 @@
+package org.java;
+
+public class java {
+	public static void main(String[] args){
+	    int num=2345;
+	    int count=0;
+	    while(num>0){
+	        count++;
+	        num=num/10;
+	    }
+	    System.out.println(count);
+		}
+}
